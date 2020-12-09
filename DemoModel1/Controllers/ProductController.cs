0ecec7d2f85b1079851473a4be524c3f6b0b8b01
Adrian Garcia <adrian1994215@gmail.com>
2020-12-08 22:41:46 -0500

    Demo clase 6/12/2020

---
+++ DemoModel1/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelCL;
+
+namespace DemoModel1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+
+
+        public ActionResult Index()
+        {
+            var Repository = new ModelCL.Repository();
+            var Model = Repository.GetProducts();
+            return View(Model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            ActionResult Result = null;
+            var Repository = new ModelCL.Repository();
+            var Model = Repository.GetProductByID(id);
+            if (Model != null)
+            {
+                Result = Content((Model.ProductID).ToString());
+                Result = Content(Model.ProductName);
+               // Result = Content(Model.ProductName);
+
+            }
+            else
+            {
+                Result = Content("Producto no encontrado");
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ ModelCL/Repository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelCL
+{
+    public class Repository
+    {
+        public Product GetProductByID(int ID)
+        {
+            var Context = new NorthwindEntities();
+            return Context.Products.FirstOrDefault(p => p.ProductID == ID);
+        }
+
+        public List<Product> GetProducts()
+        {
+            List<Product> Result = null;
+            using (var Context = new NorthwindEntities())
+            {
+                Result = Context.Products.ToList();
+            }
+            return Result;
+        }
+
+
+
+        List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+                ProductID=1,ProductName="Sal",
+                UnitPrice=20, UnitsInStock=20
+            },
+            new Product
+            {
+                ProductID=2,ProductName="Aceite",
+                UnitPrice=150, UnitsInStock=30
+            }
+        };
+    }
+}
